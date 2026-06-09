219cd99d949af15a6e52f66e6448b234e4a6e2d2
Mohammadtej <tej.mohammad53@gmail.com>
2026-06-09 23:34:48 +0530

    RDNC-MU5LLJ - block unnecessary sign-ups

--- ghost_data_garden/auditor/forms.py
+++ ghost_data_garden/auditor/forms.py
@@ -9,6 +9,19 @@ class CustomUserCreationForm(UserCreationForm):
         model = User
         fields = ("username", "email")
 
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if email:
+            domain = email.split('@')[-1].lower()
+            # Add or remove domains from this list as needed
+            disposable_domains = [
+                'mailinator.com', '10minutemail.com', 'guerrillamail.com',
+                'temp-mail.org', 'yopmail.com', 'throwawaymail.com', 'sharklasers.com'
+            ]
+            if domain in disposable_domains:
+                raise forms.ValidationError("Please use a valid, non-disposable email address.")
+        return email
+
 class CustomLoginForm(forms.Form):
     email = forms.EmailField(widget=forms.EmailInput(attrs={'autofocus': True}))
     password = forms.CharField(label="Password", strip=False, widget=forms.PasswordInput)
